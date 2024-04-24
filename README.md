f10062eace5131afee5bbfb5b45de042a4bddd77
SauRavRwT <souravrawat142@gmail.com>
2024-04-24 21:34:37 +0530

    Art-Gallary: random fixes...
    fixed crash on searched images
    updated README.md
    colors changed

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-# Getting Started with Create React App
+# Description
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+_Papers Web-App with made up of reactjs uses Pexel api to deliver high-quality wallpaper content. The website offers a wide variety of wallpapers for users to choose from, and the content is updated regularly. Additionally, the website has all the features that are necessary to be a successful wallpaper website. This includes an easy navigation system, high-quality images, and a user-friendly interface._
 
-## Available Scripts
 
-In the project directory, you can run:
+<img src="./images/p1.png" width="50%" height="50%"><img src="./images/p2.png" width="50%" height="50%">
+
+# Clone and Use
+- The website is completely built on react-js library of javascript and that's why we need nodejs and npm installed
+- While installing nodejs and npm, try to install versions which are equal or greater than the versions mentioned in badges above
+- In case you want to help developing it or simply saving it, you can fork the repository just by clicking the button on the top-right corner of this page
+- After the successful installation of nodejs and npm, clone the repository into your local system using below command:
+ ```bash
+ git clone https://github.com/SauRavRwT/Wallpaper-App.git
+ ```
+- To download required dependencies to your system, navigate to the directory where the cloned repository resides and execute following command:
+  ```node
+  npm install
+  ```
+- Now the project is ready to use
+- You can check it using `npm start`, it will open the website locally on your browser.
 
 ### `npm start`
 
@@ -14,10 +28,14 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
+### Things need to know
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+_API key -_
+
+you get it from [pexels](https://www.pexels.com/api/new/)
+```js
+    const API_KEY = "YOUR_API_KEY";
+```
 
 ### `npm run build`
 
@@ -26,45 +44,3 @@ It correctly bundles React in production mode and optimizes the build for the be
 
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
-import Logo from "./assets/Logo.png";
+// import logo from "./assets/logo.png";
 import Download from "./assets/download.png";
 
 function App() {
@@ -9,66 +9,52 @@ function App() {
   const [pageIndex, setPageIndex] = useState(1);
   const [searchValueGlobal, setSearchValueGlobal] = useState("");
 
-  useEffect(() => {
-    getImages(pageIndex);
-  }, [pageIndex]); // Reload images when pageIndex changes
+  const fetchImages = useCallback(
+    async (url) => {
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: API_KEY,
+          },
+        });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        return { photos: [] };
+      }
+    },
+    [API_KEY]
+  );
 
-  const fetchImages = async (url) => {
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: API_KEY,
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching images:", error);
-    }
-  };
+  const getImages = useCallback(
+    async (index) => {
+      const baseURL = `https://api.pexels.com/v1/curated?page=${index}&per_page=12`;
+      const data = await fetchImages(baseURL);
+      setImages(data.photos);
+    },
+    [fetchImages]
+  );
 
-  const GenerateHTML = (photos) => {
-    return photos.map((photo) => (
-      <div className="item" key={photo.id}>
-        <a
-          href={photo.src.original}
-          data-lightbox="mygallary"
-          data-title={photo.photographer}
-        >
-          <img src={photo.src.large} alt={photo.photographer} />
-          <h3>{photo.photographer}</h3>
-        </a>
-        <a
-          href={photo.src.original}
-          target="_blank"
-          download={photo.src.original}
-          rel="noreferrer"
-        >
-          <img className="photo-download_info" src={Download} alt="Download" />
-        </a>
-      </div>
-    ));
-  };
+  const getSearchedImages = useCallback(
+    async (searchValue) => {
+      const baseURL = `https://api.pexels.com/v1/search?query=${searchValue}&page=1&per_page=12`;
+      const data = await fetchImages(baseURL);
+      setImages(data.photos);
+    },
+    [fetchImages]
+  );
 
-  const getImages = async (index) => {
-    const baseURL = `https://api.pexels.com/v1/curated?page=${index}&per_page=12`;
-    const data = await fetchImages(baseURL);
-    setImages(data.photos);
-  };
-
-  const getSearchedImages = async (searchValue) => {
-    const baseURL = `https://api.pexels.com/v1/search?query=${searchValue}&page=1&per_page=12`;
-    const data = await fetchImages(baseURL);
-    setImages(data.photos);
-  };
-
-  const getMoreSearchedImages = async (index) => {
-    const baseURL = `https://api.pexels.com/v1/search?query=${searchValueGlobal}&page=${index}&per_page=12`;
-    const data = await fetchImages(baseURL);
-    setImages((prevImages) => [...prevImages, ...data.photos]);
-  };
+  const getMoreSearchedImages = useCallback(
+    async (index) => {
+      const baseURL = `https://api.pexels.com/v1/search?query=${searchValueGlobal}&page=${index}&per_page=12`;
+      const data = await fetchImages(baseURL);
+      setImages((prevImages) => [...prevImages, ...data.photos]);
+    },
+    [fetchImages, searchValueGlobal]
+  );
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -80,23 +66,54 @@ function App() {
   };
 
   const handleLoadMore = () => {
-    setPageIndex((prevIndex) => prevIndex + 1);
+    if (searchValueGlobal) {
+      getMoreSearchedImages(pageIndex + 1);
+    } else {
+      setPageIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    getImages(pageIndex);
+  }, [pageIndex, getImages]);
+
+  const GenerateHTML = (photos) => {
+    return photos.map((photo) => (
+      <div className="item" key={photo.id}>
+        <a
+          href={photo.src.original}
+          data-lightbox="Gallery"
+          data-title={photo.photographer}
+        >
+          <img src={photo.src.large} alt={photo.photographer} />
+          <h3>{photo.photographer}</h3>
+        </a>
+        <a
+          href={photo.src.original}
+          target="_blank"
+          download={photo.src.original}
+          rel="noopener noreferrer"
+        >
+          <img className="photo-download_info" src={Download} alt="Download" />
+        </a>
+      </div>
+    ));
   };
 
   return (
     <section>
       <div className="container">
         <header className="header">
-          <img src={Logo} />
+          {/* <img src={logo} alt="logo" /> */}
+          <h1>Art-Gallery</h1>
           <form onSubmit={handleSearch}>
             <input type="text" placeholder="Search" />
-            <ion-icon name="search-outline"></ion-icon>
           </form>
         </header>
         <div className="gallery">{GenerateHTML(images)}</div>
-        <a className="load-more" onClick={handleLoadMore}>
+        <button className="load-more" onClick={handleLoadMore}>
           Load More
-        </a>
+        </button>
       </div>
     </section>
   );
