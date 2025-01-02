3a8ad55ab31dbb96bd8f58cc87ec714acf4c4a61
SauRavRwT <souravrawat142@gmail.com>
2025-01-02 20:48:29 +0530

    Updates

---
+++ precache-manifest.8a09f7cf0d11f6f39d8e80547048e455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9306811056bfc087acf03fdd0ba1b928",
+    "url": "/Wallpaper-App/index.html"
+  },
+  {
+    "revision": "2349392b5a5faa8c1480",
+    "url": "/Wallpaper-App/static/css/main.6493f17a.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/Wallpaper-App/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Wallpaper-App/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "2349392b5a5faa8c1480",
+    "url": "/Wallpaper-App/static/js/main.4ab08539.chunk.js"
+  },
+  {
+    "revision": "552cf812c1736199f175",
+    "url": "/Wallpaper-App/static/js/runtime~main.386815fa.js"
+  }
+]);
